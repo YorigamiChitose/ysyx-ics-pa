84060d805ed29ed8bd36215a96629689db3419c8
tracer-ysyx <tracer@ysyx.org>
2024-04-12 13:54:12 +0800

    > compile NEMU ysyx_22050990 YorigamiChitose Linux DESKTOP-7H30M6U 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:54:12 up  2:54,  1 user,  load average: 0.23, 0.23, 0.15

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,8 +17,36 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+extern const char *regs[];
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  if (ref_r->pc != cpu.pc) {
+    Log("Different values of the PC! REF: " FMT_PADDR " DUT: " FMT_PADDR , ref_r->pc, cpu.pc);
+    return false;
+  }
+  for (int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++) {
+    if (ref_r->gpr[i] != gpr(i)) {
+      Log("Different values of reg %s! REF: " FMT_WORD " DUT: " FMT_WORD, regs[i], ref_r->gpr[i], gpr(i));
+      return false;
+    }
+  }
+  if (ref_r->csr.mepc != cpu.csr.mepc) {
+    Log("Different values of csr mepc! REF: " FMT_WORD " DUT: " FMT_WORD, ref_r->csr.mepc, cpu.csr.mepc);
+    return false;
+  }
+  if (ref_r->csr.mcause != cpu.csr.mcause) {
+    Log("Different values of csr mcause! REF: " FMT_WORD " DUT: " FMT_WORD, ref_r->csr.mcause, cpu.csr.mcause);
+    return false;
+  }
+  if (ref_r->csr.mstatus != cpu.csr.mstatus) {
+    Log("Different values of csr mstatus! REF: " FMT_WORD " DUT: " FMT_WORD, ref_r->csr.mstatus, cpu.csr.mstatus);
+    return false;
+  }
+  if (ref_r->csr.mtvec != cpu.csr.mtvec) {
+    Log("Different values of csr mtvec! REF: " FMT_WORD " DUT: " FMT_WORD, ref_r->csr.mtvec, cpu.csr.mtvec);
+    return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
