7aee48ab5ed81cb49105b32a9d3a108886903580
tracer-ysyx <tracer@ysyx.org>
2024-04-12 22:06:17 +0800

    > compile NEMU ysyx_22050990 YorigamiChitose Linux DESKTOP-7H30M6U 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:06:17 up 10:10,  1 user,  load average: 0.35, 0.35, 0.21

--- navy-apps/tests/time-test/time.c
+++ navy-apps/tests/time-test/time.c
@@ -7,11 +7,11 @@
 int main() {
   NDL_Init(0);
   uint64_t last_time = NDL_GetTicks();
-  printf("%llu\n", last_time);
+  // printf("%llu\n", last_time);
   while (1) {
     uint64_t time_ms = NDL_GetTicks();
     if(time_ms > last_time + 500) {
-    printf("%llu\n", time_ms);
+      // printf("%llu\n", time_ms);
       last_time = time_ms;
       printf("FUCK!\n");
     }
