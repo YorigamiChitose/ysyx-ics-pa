41ba857adc3e4f0ccaaa57fc0cc449faadf61ca1
tracer-ysyx <tracer@ysyx.org>
2024-04-12 23:21:02 +0800

    > compile NEMU ysyx_22050990 YorigamiChitose Linux DESKTOP-7H30M6U 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:21:02 up 11:24,  1 user,  load average: 0.17, 0.15, 0.16

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,7 +37,7 @@ void init_proc() {
   // naive_uload(NULL, "/bin/nterm");
   // naive_uload(NULL, "/bin/bird");
   // naive_uload(NULL, "/bin/pal");
-  naive_uload(NULL, "/bin/am-kernels");
+  naive_uload(NULL, "/bin/coremark");
   // naive_uload(NULL, "/bin/fceux");
 }
 
