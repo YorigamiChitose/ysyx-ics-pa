827e2fab07b2321b83f4908cfd7f6d4b703a4b86
tracer-ysyx <tracer@ysyx.org>
2024-04-12 21:47:56 +0800

    > compile NEMU ysyx_22050990 YorigamiChitose Linux DESKTOP-7H30M6U 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:47:56 up  9:51,  1 user,  load average: 0.18, 0.13, 0.11

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -4,6 +4,7 @@
 
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
+void naive_uload(PCB *pcb, const char *filename);
 PCB *current = NULL;
 
 void switch_boot_pcb() {
@@ -25,7 +26,18 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-
+  naive_uload(NULL, "/bin/dummy");
+  // naive_uload(NULL, "/bin/hello");
+  // naive_uload(NULL, "/bin/time-test");
+  // naive_uload(NULL, "/bin/file-test");
+  // naive_uload(NULL, "/bin/event-test");
+  // naive_uload(NULL, "/bin/bmp-test");
+  // naive_uload(NULL, "/bin/nslider");
+  // naive_uload(NULL, "/bin/menu");
+  // naive_uload(NULL, "/bin/nterm");
+  // naive_uload(NULL, "/bin/bird");
+  // naive_uload(NULL, "/bin/pal");
+  // naive_uload(NULL, "/bin/fceux");
 }
 
 Context* schedule(Context *prev) {
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,61 @@
 #include <common.h>
+#include <fs.h>
+#include <sys/time.h>
 #include "syscall.h"
+
+void trace_syscall(uintptr_t code) {
+  switch (code) {
+    case -1: Debug("System call: yield"); break;
+    case SYS_exit: Debug("System call: SYS_exit"); break;
+    case SYS_yield: Debug("System call: SYS_yield"); break;
+    case SYS_open: Debug("System call: SYS_open"); break;
+    case SYS_read: Debug("System call: SYS_read"); break;
+    case SYS_write: Debug("System call: SYS_write"); break;
+    case SYS_close: Debug("System call: SYS_close"); break;
+    case SYS_lseek: Debug("System call: SYS_lseek"); break;
+    case SYS_brk: Debug("System call: SYS_brk"); break;
+    case SYS_gettimeofday: Debug("System call: SYS_gettimeofday"); break;
+    default: Debug("System call: Error"); break;
+  }
+}
+
+intptr_t mybrk(uintptr_t increment) {
+  return 0;
+}
+
+int mygettimeofday(struct timeval *tv, struct timezone *tz) {
+  AM_TIMER_UPTIME_T t = io_read(AM_TIMER_UPTIME);
+  if (tv) {
+    tv->tv_usec = t.us % (1000 * 1000);
+    tv->tv_sec = t.us / (1000 * 1000);
+  }
+  if (tz) {
+    tz->tz_minuteswest = -480;
+    tz->tz_dsttime = 0;
+  }
+  return 0;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+
+  trace_syscall(a[0]);
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+    case -1: Log("yield"); break;
+    case SYS_exit: halt(a[1]); break;
+    case SYS_yield: Log("yield"); break;
+    case SYS_open: c->GPRx = fs_open((const char *)a[1], a[2], a[3]); break;
+    case SYS_read: c->GPRx = fs_read(a[1], (void *)a[2], a[3]); break;
+    case SYS_write: c->GPRx = fs_write(a[1], (const char *)a[2], a[3]); break;
+    case SYS_close: c->GPRx = fs_close(a[1]); break;
+    case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
+    case SYS_brk: c->GPRx = mybrk(a[1]); break;
+    case SYS_gettimeofday: c->GPRx = mygettimeofday((struct timeval *)a[1], (struct timezone *)a[2]); break;
+    default: panic("Unhandled syscall ID = %d", a[0]); break;
   }
 }
