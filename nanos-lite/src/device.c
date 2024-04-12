b057e5cbce21a99a60d2811f31e33e04c536400a
tracer-ysyx <tracer@ysyx.org>
2024-04-12 21:39:18 +0800

    > compile NEMU ysyx_22050990 YorigamiChitose Linux DESKTOP-7H30M6U 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:39:18 up  9:43,  1 user,  load average: 0.21, 0.22, 0.14

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,19 +15,32 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  int i = 0;
+  while (i != len) {
+    putch(((char *)buf)[i++]);
+  }
+  return len;
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  AM_INPUT_KEYBRD_T key = io_read(AM_INPUT_KEYBRD);
+  if (key.keycode == AM_KEY_NONE) {
+    return -1;
+  }
+  return snprintf(buf, len, "k%c %s\n", key.keydown ? 'd' : 'u', keyname[key.keycode]);
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
+  return snprintf(buf, len, "WIDTH : %d\nHEIGHT : %d\n", gpu.width, gpu.height);
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  AM_GPU_CONFIG_T cfg = io_read(AM_GPU_CONFIG);
+  int x = offset / sizeof(uint32_t) % cfg.width;
+  int y = offset / sizeof(uint32_t) / cfg.width;
+  io_write(AM_GPU_FBDRAW, x, y, (void*)buf, len/sizeof(uint32_t), 1, true);
+  return len;
 }
 
 void init_device() {
