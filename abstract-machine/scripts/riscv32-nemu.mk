116ea7266fd802248b07045e59c9aaa2ae76b1f8
tracer-ysyx <tracer@ysyx.org>
2024-04-14 11:52:15 +0800

    > compile NEMU ysyx_22050990 YorigamiChitose Linux DESKTOP-7H30M6U 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:52:15 up 43 min,  1 user,  load average: 0.23, 0.15, 0.10

--- abstract-machine/scripts/riscv32-nemu.mk
+++ abstract-machine/scripts/riscv32-nemu.mk
@@ -8,9 +8,3 @@ AM_SRCS += riscv/nemu/start.S \
            riscv/nemu/cte.c \
            riscv/nemu/trap.S \
            riscv/nemu/vme.c
-
-AM_SRCS += riscv/nemu/libgcc/div.S \
-           riscv/nemu/libgcc/muldi3.S \
-           riscv/nemu/libgcc/multi3.c \
-           riscv/nemu/libgcc/ashldi3.c \
-           riscv/nemu/libgcc/unused.c
