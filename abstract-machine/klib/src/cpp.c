bc1bf579d3e0298b1c6a1c519f16094e038ebe45
YorigamiChitose <YorigamiChitose@outlook.com>
2024-07-05 18:34:39 +0800

    🐞 fix(nemu abstract-machine): nemu in riscv32e-nemu

--- abstract-machine/klib/src/cpp.c
+++ abstract-machine/klib/src/cpp.c
@@ -16,4 +16,32 @@ void __cxa_atexit() {
   assert(0);
 }
 
+uint64_t __lshrdi3(uint64_t a, unsigned int shift) {
+    if (shift == 0) {
+        return a;
+    } else if (shift >= 64) {
+        return 0;
+    } else if (shift >= 32) {
+        // Shift more than 32 but less than 64
+        return (uint64_t)(a >> 32) >> (shift - 32);
+    } else {
+        // General case
+        return (a >> shift) | ((uint64_t)((uint32_t)a >> (shift - 32)) << (32 - shift));
+    }
+}
+
+int64_t __ashldi3(int64_t a, unsigned int shift) {
+    if (shift == 0) {
+        return a;
+    } else if (shift >= 64) {
+        return 0;
+    } else if (shift >= 32) {
+        // Shift more than 32 but less than 64
+        return (int64_t)((uint64_t)(a << (shift - 32)) << 32);
+    } else {
+        // General case
+        return (a << shift) | ((uint64_t)((uint32_t)(a >> (32 - shift))) << 32);
+    }
+}
+
 #endif
